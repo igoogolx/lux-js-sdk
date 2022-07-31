358a2de5676e1bd5b264e8d2c10ca165f6751ce4
igoogolx <27353191+igoogolx@users.noreply.github.com>
2022-07-31 02:22:03 -0700

    feat: eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: ["airbnb-typescript-prettier"],
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "import/prefer-default-export": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
